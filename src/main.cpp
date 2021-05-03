01b30aaf3e58ed09d056862c8fd65b9416e7d95f
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2021-05-03 14:46:37 +0530

    remove explicit high DPI scaling attrib

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon("://icon"));
 
